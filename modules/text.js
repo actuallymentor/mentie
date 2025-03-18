ef9c065447884e895ed0e51602e1d7aa4351addb
Mentor <mentor@palokaj.co>
2025-03-18 10:59:03 +0100

    ✨ random strings

--- modules/text.js
+++ modules/text.js
@@ -1,3 +1,5 @@
+import { random_number_between } from "./numbers"
+
 /**
  * Truncates a given text to a specified length and appends a suffix if necessary.
  *
@@ -37,4 +39,31 @@ export const copy_to_clipboard = async ( text, { success_message='Copied to clip
 export const capitalise = string => { 
     if( !string ) return ''
     return `${ string?.charAt( 0 ).toUpperCase() }${ string?.slice( 1, string?.length ) }`
+}
+
+
+/**
+ * Generates a random letter from the allowed characters.
+ *
+ * @param {string} [allowed_chars] - A string of characters to choose from. If not provided, defaults to the lowercase English alphabet.
+ * @returns {string} A randomly selected character from the allowed characters.
+ */
+export const random_letter = ( allowed_chars, capitals=true ) => {
+
+    const letters = 'abcdefghijklmnopqrstuvwxyz'
+    let chars = allowed_chars || letters
+    if( capitals ) chars += letters.toUpperCase()
+
+    return chars.charAt( Math.floor( Math.random() * chars.length ) )
+
+}
+
+export const random_string_of_length = ( length, allowed_chars, numbers=true ) => {
+
+    return Array.from( { length }, () => {
+        // If numbers are allowed, add a 50% chance of generating a number
+        if( numbers && Math.random() > 0.5 ) return `${ random_number_between( 9, 0 ) }`
+        return random_letter( allowed_chars )
+    } ).join( '' )
+
 }
