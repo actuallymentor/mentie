eb6538448e1af3f7d22ae03331e0b5e3e527c3e3
Mentor <mentor@palokaj.co>
2025-09-02 12:56:53 +0200

    ✨ array shuffle in-place option

--- modules/manipulations.js
+++ modules/manipulations.js
@@ -3,17 +3,21 @@
  * @param {Array} array 
  * @returns {Array} The shuffled array.
  */
-export const shuffle_array = array => {
+export const shuffle_array = ( array, in_place=true ) => {
+
 
     // Validate input
     if( Array.isArray( array ) === false ) throw new Error( 'Input must be an array' )
 
+    // Handle in-place shuffling
+    const to_shuffle = in_place ? array : [ ...array ]
+
     // Fisher-Yates shuffle 
-    for( let i = array.length - 1; i > 0; i-- ) {
+    for( let i = to_shuffle.length - 1; i > 0; i-- ) {
         const j = Math.floor( Math.random() * ( i + 1 ) );
-        [ array[i], array[j] ] = [ array[j], array[i] ]
+        [ to_shuffle[i], to_shuffle[j] ] = [ to_shuffle[j], to_shuffle[i] ]
     }
 
-    return array
+    return to_shuffle
 
 }
